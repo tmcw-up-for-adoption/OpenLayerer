10e5e492ced8cc5a81ae11d191214f467a89a658
Tom MacWright <macwright@gmail.com>
2009-09-10 17:45:55 -0400

    Removing build directory, switching to pending-patch version of build.py

--- build.py
+++ build.py
@@ -1,31 +1,54 @@
 #!/usr/bin/env python
 
 import sys
-#sys.path.append("../tools")
+sys.path.append("../tools")
 import mergejs
 
-have_compressor = None
-try:
-    import jsmin
-    have_compressor = "jsmin"
-except ImportError:
+source_directory = "../lib"
+config_filename = "full.cfg"
+output_filename = "OpenLayers.js"
+
+def minimize(text):
     try:
+        import jsmin
+        print "Compressing using jsmin."
+        return license + jsmin.jsmin(text)
+    except Exception, e:
         import minimize
-        have_compressor = "minimize"
-    except Exception, E:
-        print E
-        pass
+        print "Compressing using minimize."
+        return license + minimize.minimize(text)
 
-def process(config):
-    sourceDirectory = "./lib"
-    merged = mergejs.run(sourceDirectory, None, config)
-    if have_compressor == "jsmin":
-        minimized = jsmin.jsmin(merged)
-    elif have_compressor == "minimize":
-        minimized = minimize.minimize(merged)
-    else: # fallback
-        minimized = merged 
-    minimized = file("license.txt").read() + minimized
-    return minimized
+def build(config_filename, output_filename, compress = True, **kwargs):
+    merged = mergejs.run(kwargs.get('source_directory', source_directory), None, config_filename)
+    if compress:
+        try:
+            output = minimize(merged)
+        except Exception, e:
+            print "Neither mergejs nor minimize was found for compression.\
+                Install one of these modules or specify --nocompress"
+    else:
+        output = merged
+    license = file(kwargs.get('license', 'license.txt')).read() 
+    return output
 
-#file(outputFilename, "w").write(minimized)
+if __name__ == "__main__":
+    from optparse import OptionParser
+    usage = "usage: build.py config output [--nocompress don't compress output]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-N', '--nocompress', dest="compress", action="store_false",
+        default=True, help="Do not compress output. Not recommended for production.")
+    (options, args) = parser.parse_args()
+
+    license = file("license.txt").read() 
+
+    try:
+        config_filename = sys.argv[1]
+        output_filename = sys.argv[2]
+    except:
+        parser.print_help()
+        sys.exit()
+
+    output = build(config_filename, output_filename, options.compress)
+    
+    print "Writing to %s." % output_filename
+    file(output_filename, "w").write(output)
